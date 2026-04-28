a67d750d8c0e6de4a34708b7a938f93e399cd348
nkiyo <nakahara.kiyoshi@gmail.com>
2026-04-28 20:55:33 +0900

    init commit

---
+++ script.ts
@@ -0,0 +1,47 @@
+import { prisma } from "./lib/prisma";
+
+async function main() {
+  // Create user
+  // const user = await prisma.user.create({
+  //   data: {
+  //     name: "Alice",
+  //     email: "alice@prisma.io",
+  //   },
+  // });
+  // console.log("Created user:", user);
+
+  // Create post
+  // const post = await prisma.post.create({
+  //   data: {
+  //     title: "ほげ",
+  //     authorId: 2
+  //   }
+  // })
+  // console.log(post)
+
+  // Delete User
+  // cascadeの場合はPostも全部消える
+  const u = await prisma.user.delete({
+    where: {
+      id: 2,
+    }
+  })
+  console.log(u)
+
+  // const p = await prisma.post.delete({
+  //   where: {
+  //     id: 3
+  //   }
+  // })
+  // console.log(p)
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
